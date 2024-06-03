650a7ea75cb5d543e0edb499c37d15c434553218
DanhTai <danhtai97tb@gmail.com>
2024-06-03 22:02:08 +0700

    Update apigate way

--- server/weather-forecast-microservices/ApiGateways/YarpApiGateway/Program.cs
+++ server/weather-forecast-microservices/ApiGateways/YarpApiGateway/Program.cs
@@ -1,7 +1,11 @@
 using Microsoft.AspNetCore.RateLimiting;
+using Ocelot.Middleware;
+using Ocelot.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
+IConfiguration configuration = new ConfigurationBuilder()
+                                    .AddJsonFile("ocelot.json").Build();
 // Add services to the container.
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
@@ -15,11 +19,20 @@ builder.Services.AddRateLimiter(rateLimiterOptions =>
     });
 });
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddOcelot(configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseRateLimiter();
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+app.UseOcelot();
 
 app.MapReverseProxy();
-
 app.Run();
