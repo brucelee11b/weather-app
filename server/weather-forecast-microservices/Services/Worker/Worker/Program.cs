cd24893d3f8859507f20a98b3770619fb308c900
DESKTOP-9G8557Q\Lnhie <HieuLNGBH19006@fpt.com.vn>
2024-06-12 23:06:20 +0700

    [HieuLN8] add source get dailly data

--- server/weather-forecast-microservices/Services/Worker/Worker/Program.cs
+++ server/weather-forecast-microservices/Services/Worker/Worker/Program.cs
@@ -1,16 +1,17 @@
-using Worker;
+﻿using Worker;
 using Worker.Repository;
 using Microsoft.Extensions.Caching.Distributed;
 using StackExchange.Redis;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Workers>();
+builder.Services.AddHttpClient(); // Đăng ký IHttpClientFactory
 
 builder.Services.AddSingleton<ICaching, Caching>();
 builder.Services.AddMemoryCache();
-builder.Services.AddSingleton<IDistributedCache, MemoryDistributedCache>();
+builder.Services.AddSingleton<IDistributedCache, MemoryDistributedCache>(); 
 builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
-    ConnectionMultiplexer.Connect(builder.Configuration.GetConnectionString("RedisConfiguration")));
+    ConnectionMultiplexer.Connect("localhost:6379"));
 
 var host = builder.Build();
 host.Run();
--- server/weather-forecast-microservices/Services/Worker/Worker/Workers.cs
+++ server/weather-forecast-microservices/Services/Worker/Worker/Workers.cs
@@ -1,12 +1,28 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Worker.Repository;
+
 namespace Worker
 {
     public class Workers : BackgroundService
     {
         private readonly ILogger<Workers> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly string apiKey = "a138902a1200a09b2c57e0119815c601";
+        private readonly ICaching _caching;
 
-        public Workers(ILogger<Workers> logger)
+        const string currentWeatherQueue = "current-weather-queue";
+        const string weatherForecastQueue = "weather-forecast-queue";
+
+        public Workers(ILogger<Workers> logger, IHttpClientFactory httpClientFactory, ICaching caching)
         {
-            _logger = logger;
+            _logger = logger; 
+            _httpClientFactory = httpClientFactory;
+            _caching = caching;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -17,7 +33,40 @@ namespace Worker
                 {
                     _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 }
-                await Task.Delay(1000, stoppingToken);
+
+                // Gọi API khi khởi động
+                await CallApiAsync("21.0294498", "105.8544441", "10");
+
+                await Task.Delay(5000, stoppingToken);
+            }
+        }
+
+        private async Task CallApiAsync(string latitude, string longitude, string count)
+        {
+            using (var client = _httpClientFactory.CreateClient())
+            {
+                try
+                {
+                    var version = "data/2.5/";
+                    StringBuilder url = new StringBuilder();
+                    url.Append($"{version}forecast?lat={latitude}&lon={longitude}&cnt={count}&appid={apiKey}&units=metric");
+
+                    client.BaseAddress = new Uri("https://api.openweathermap.org/");
+                    HttpResponseMessage response = await client.GetAsync(url.ToString());
+                    response.EnsureSuccessStatusCode();
+                    var responseData = await response.Content.ReadAsStringAsync();
+
+                    var result = this._caching.SetCacheResponse("GetDailyWeatherData", responseData);
+                    if (!result)
+                    {
+                        Exception exception = new Exception("Set Cache Response Failed");
+                        throw exception;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error calling API: {e.Message}");
+                }
             }
         }
     }
