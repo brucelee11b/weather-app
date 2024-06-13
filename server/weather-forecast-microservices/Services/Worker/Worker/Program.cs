5a4125d91206462e3ede05fa22d70a9f9f18051b
DanhTai <danhtai97tb@gmail.com>
2024-06-13 22:35:41 +0700

    Update src

--- server/weather-forecast-microservices/Services/Worker/Worker/Program.cs
+++ server/weather-forecast-microservices/Services/Worker/Worker/Program.cs
@@ -2,6 +2,7 @@
 using Worker.Repository;
 using Microsoft.Extensions.Caching.Distributed;
 using StackExchange.Redis;
+using Microsoft.Extensions.Configuration;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Workers>();
@@ -12,6 +13,10 @@ builder.Services.AddMemoryCache();
 builder.Services.AddSingleton<IDistributedCache, MemoryDistributedCache>(); 
 builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
     ConnectionMultiplexer.Connect("localhost:6379"));
-
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+	options.Configuration = "localhost:6379";
+	options.InstanceName = "SampleInstance"; // Optional instance name
+});
 var host = builder.Build();
 host.Run();
