39b931978339fe855428e80eff7fe6f6a93f9c36
DESKTOP-9G8557Q\Lnhie <HieuLNGBH19006@fpt.com.vn>
2024-06-16 12:37:37 +0700

    [HieuLN8] update source data by provinces

---
+++ server/weather-forecast-microservices/Services/Worker/Worker/Provinces.cs
@@ -0,0 +1,450 @@
+﻿namespace Worker
+{
+    public class Provinces
+    {
+        public List<object> provinces = new List<object>()
+                {
+                    new
+                    {
+                        name = "HaNoi",
+                        lat = "21.0245",
+                        lon = "105.8412",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "Hagiang",
+                        lat = "22.8274",
+                        lon = "104.9866",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "CaoBang",
+                        lat = "22.6667",
+                        lon = "106.2588",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "LaoCai",
+                        lat = "22.4997",
+                        lon = "103.9657",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "SonLa",
+                        lat = "21.3283",
+                        lon = "103.9005",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "LaiChau",
+                        lat = "22.3997",
+                        lon = "103.4517",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "BacKan",
+                        lat = "22.1495",
+                        lon = "105.8372",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "LangSon",
+                        lat = "21.8505",
+                        lon = "106.7662",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "TuyenQuang",
+                        lat = "21.8233",
+                        lon = "105.2181",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "YenBai",
+                        lat = "21.7026",
+                        lon = "104.8714",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "ThaiNguyen",
+                        lat = "21.5928",
+                        lon = "105.8442",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "DienBien",
+                        lat = "21.3833",
+                        lon = "103.0167",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "PhuTho",
+                        lat = "11.5486",
+                        lon = "109.0281",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "VinhPhuc",
+                        lat = "18.4169",
+                        lon = "105.6954",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "BacGiang",
+                        lat = "21.2779",
+                        lon = "106.1939",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "BacNinh",
+                        lat = "21.1833",
+                        lon = "106.05",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "QuangNinh",
+                        lat = "20.9528",
+                        lon = "107.08",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "HaiDuong",
+                        lat = "20.9333",
+                        lon = "106.3167",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "HaiPhong",
+                        lat = "20.8561",
+                        lon = "106.6822",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "HoaBinh",
+                        lat = "20.813",
+                        lon = "105.3453",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "HungYen",
+                        lat = "20.6491",
+                        lon = "106.051",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "HaNam",
+                        lat = "16.3358",
+                        lon = "107.7706",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "ThaiBinh",
+                        lat = "20.4493",
+                        lon = "106.3425",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "NamDinh",
+                        lat = "20.423",
+                        lon = "106.1684",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "NinhBinh",
+                        lat = "20.2545",
+                        lon = "105.9765",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "ThanhHoa",
+                        lat = "19.8",
+                        lon = "105.7667",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "NgheAn",
+                        lat = "18.6667",
+                        lon = "105.6667",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "HaTinh",
+                        lat = "18.3453",
+                        lon = "105.9019",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "QuangBinh",
+                        lat = "17.4833",
+                        lon = "106.6",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "QuangTri",
+                        lat = "16.75",
+                        lon = "107.2",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "Hue",
+                        lat = "16.4667",
+                        lon = "107.6",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "Danang",
+                        lat = "16.0678",
+                        lon = "108.2208",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "QuangNam",
+                        lat = "15.5757",
+                        lon = "108.4743",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "QuangNgai",
+                        lat = "15.1167",
+                        lon = "108.8",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "KonTum",
+                        lat = "14.35",
+                        lon = "108",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "GiaLai",
+                        lat = "13.75",
+                        lon = "108.25",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "BinhDinh",
+                        lat = "13.886",
+                        lon = "109.1117",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "PhuYen,",
+                        lat = "13.0833",
+                        lon = "109.3",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "DakLak",
+                        lat = "12.6667",
+                        lon = "108.05",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "KhanhHoa",
+                        lat = "10.6765",
+                        lon = "105.1903",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "DakNong",
+                        lat = "12.0085",
+                        lon = "107.6901",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "LamDong",
+                        lat = "11.9402",
+                        lon = "108.4376",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "NinhThuan",
+                        lat = "11.5653",
+                        lon = "108.9886",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "BinhPhuoc",
+                        lat = "11.5333",
+                        lon = "106.9167",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "TayNinh",
+                        lat = "11.3135",
+                        lon = "106.096",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "BinhDuong",
+                        lat = "14.2972",
+                        lon = "109.0797",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "DongNai",
+                        lat = "10.9569",
+                        lon = "106.8536",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "BinhThuan",
+                        lat = "10.9333",
+                        lon = "108.1",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "HoChiMinh",
+                        lat = "10.75",
+                        lon = "106.6667",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "LongAn",
+                        lat = "10.6667",
+                        lon = "106.1667",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "BaRiaVungTau",
+                        lat = "10.4963",
+                        lon = "107.1688",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "DongThap",
+                        lat = "10.462",
+                        lon = "105.6358",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "AnGiang",
+                        lat = "10.5",
+                        lon = "105.1667",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "TienGiang",
+                        lat = "10.35",
+                        lon = "106.35",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "VinhLong",
+                        lat = "10.2544",
+                        lon = "105.967",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "BenTre",
+                        lat = "10.2448",
+                        lon = "1106.3735",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "CanTho",
+                        lat = "10.0333",
+                        lon = "105.7833",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "KienGiang",
+                        lat = "10.019",
+                        lon = "105.0822",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "TraVinh",
+                        lat = "9.9356",
+                        lon = "106.3398",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "HauGiang",
+                        lat = "9.7579",
+                        lon = "105.6410",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "SocTrang",
+                        lat = "9.6081",
+                        lon = "105.9715",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "BacLieu",
+                        lat = "9.2825",
+                        lon = "105.7261",
+                        seq = "10",
+                    },
+                    new
+                    {
+                        name = "CaMau",
+                        lat = "9.1792",
+                        lon = "105.1458",
+                        seq = "10",
+                    }
+                };
+    }
+}
